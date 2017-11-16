568deb1eddf357750255ca55c75ddd2e7e0c5422
surov-kostya <31765707+surov-kostya@users.noreply.github.com>
2017-11-16 16:14:55 +0300

    Add JS for accordion

--- burgers/js/main.js
+++ burgers/js/main.js
@@ -11,4 +11,37 @@ let hamburgerExitBtn = document.querySelector('.exit-btn');
 
 hamburgerExitBtn.addEventListener('click', () => {
     hamburgerMenu.style.zIndex = -1;
+});
+
+// ------- TEAM ACCORDION WITH VANILLA JS -----------------
+
+let personNameBtns = document.querySelectorAll('.persons__link');
+let personItems = document.querySelectorAll('.persons__item');
+
+
+
+for(let i = 0; i < personNameBtns.length; i++){
+    personNameBtns[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        // REMOVE ACTIVE-CLASS FOR ALL ITEMS AFTER CLICK
+        for(let x = 0; x < personItems.length; x++){
+        personItems[x].classList.remove('persons__item_active');
+        }
+        // ADD ACTIVE-CLASS FOR CHOSEN ITEM
+        e.currentTarget.parentNode.classList.toggle('persons__item_active');
+        });
+    };
+
+
+// --------- MENU ACCORDION WITH JQUERY -------------------
+
+
+
+$('.menu-acco__link').on('click', (e)=>{
+    e.preventDefault();
+    // REMOVE ACTIVE-CLASS FOR ALL ITEMS AFTER CLICK
+    $('.menu-acco__item').removeClass('menu-acco__item_active');
+    // ADD ACTIVE-CLASS FOR CHOSEN ITEM
+    $(e.currentTarget).parent().toggleClass('menu-acco__item_active');
 })
+
