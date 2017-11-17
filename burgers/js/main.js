3d7a31346841e44b7385169640fa75e611039abb
surov-kostya <31765707+surov-kostya@users.noreply.github.com>
2017-11-17 15:01:11 +0300

    Add burgers section

--- burgers/js/main.js
+++ burgers/js/main.js
@@ -1,47 +1,63 @@
 // --------------- HAMBURGER MENU ------------------
 
-let hamburgerBtn = document.querySelector('.hamburger');
-let hamburgerMenu = document.querySelector('.fullscreen-menu');
+$(function(){
 
-hamburgerBtn.addEventListener('click', () => {
-    hamburgerMenu.style.display = 'flex';
-});
+    let hamburgerBtn = document.querySelector('.hamburger');
+    let hamburgerMenu = document.querySelector('.fullscreen-menu');
 
-let hamburgerExitBtn = document.querySelector('.exit-btn');
+    hamburgerBtn.addEventListener('click', () => {
+        hamburgerMenu.style.display = 'flex';
+    });
 
-hamburgerExitBtn.addEventListener('click', () => {
-    hamburgerMenu.style.display = 'none';
+    let hamburgerExitBtn = document.querySelector('.exit-btn');
+
+    hamburgerExitBtn.addEventListener('click', () => {
+        hamburgerMenu.style.display = 'none';
+    });
+
 });
 
 // ------- TEAM ACCORDION WITH VANILLA JS -----------------
 
-let personNameBtns = document.querySelectorAll('.persons__link');
-let personItems = document.querySelectorAll('.persons__item');
+$(function(){
+
+    let personNameBtns = document.querySelectorAll('.persons__link');
+    let personItems = document.querySelectorAll('.persons__item');
 
 
 
-for(let i = 0; i < personNameBtns.length; i++){
-    personNameBtns[i].addEventListener('click', (e) => {
-        e.preventDefault();
-        // REMOVE ACTIVE-CLASS FOR ALL ITEMS AFTER CLICK
-        for(let x = 0; x < personItems.length; x++){
-        personItems[x].classList.remove('persons__item_active');
-        }
-        // ADD ACTIVE-CLASS FOR CHOSEN ITEM
-        e.currentTarget.parentNode.classList.toggle('persons__item_active');
-        });
-    };
+    for(let i = 0; i < personNameBtns.length; i++){
+        personNameBtns[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            // REMOVE ACTIVE-CLASS FOR ALL ITEMS AFTER CLICK
+            for(let x = 0; x < personItems.length; x++){
+            personItems[x].classList.remove('persons__item_active');
+            }
+            // ADD ACTIVE-CLASS FOR CHOSEN ITEM
+            e.currentTarget.parentNode.classList.toggle('persons__item_active');
+            });
+        };
 
+});
 
 // --------- MENU ACCORDION WITH JQUERY -------------------
 
 
+$(function(){
+    $('.menu-acco__link').on('click', (e)=>{
+        e.preventDefault();
+        // REMOVE ACTIVE-CLASS FOR ALL ITEMS AFTER CLICK
+        $('.menu-acco__item').removeClass('menu-acco__item_active');
+        // ADD ACTIVE-CLASS FOR CHOSEN ITEM
+        $(e.currentTarget).parent().toggleClass('menu-acco__item_active');
+    });
+});
 
-$('.menu-acco__link').on('click', (e)=>{
-    e.preventDefault();
-    // REMOVE ACTIVE-CLASS FOR ALL ITEMS AFTER CLICK
-    $('.menu-acco__item').removeClass('menu-acco__item_active');
-    // ADD ACTIVE-CLASS FOR CHOSEN ITEM
-    $(e.currentTarget).parent().toggleClass('menu-acco__item_active');
-})
+// ---------- BURGER CONSISTANCE TABLE TOGGLE ---------------
 
+$(function(){
+    $('.consist__btn').on('click', (e) =>{
+        e.preventDefault();
+        $('.consist-table').toggle();
+    });
+});
